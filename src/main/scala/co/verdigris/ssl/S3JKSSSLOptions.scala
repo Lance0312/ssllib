b369a0524868885b0bd120bced0f12d7264caf10
Andrew Jo <andrewjo@gmail.com>
2017-04-24 15:02:54 -0700

    Simplify S3JKSSSLOptions
    Constructors are set up more in line with how things are done in
    Scala which greatly simplifies the code (S3JKSSSLOptions class itself
    has been reduced from 112 lines to 99 LOC).
    
    Tests have been modified.
    
    [#144138379]

--- src/main/scala/co/verdigris/ssl/S3JKSSSLOptions.scala
+++ src/main/scala/co/verdigris/ssl/S3JKSSSLOptions.scala
@@ -20,26 +20,30 @@ import scala.util.{Failure, Success, Try}
   * Can be instantiated directly with S3 URL and the Java KeyStore password or through environment variables which is
   * useful if accessing from AWS Lambda.
   */
-class S3JKSSSLOptions extends SSLOptions {
-  var s3URLEnvVarKey: String = "CASSANDRA_S3_TRUST_STORE_URL"
-  var jksPasswordEnvVarKey: String = "CASSANDRA_TRUST_STORE_PASSWORD"
-  protected var _s3url: Option[String] = None
-  private var _jksPassword: Option[String] = None
-  var awsRegion: Option[String] = None
+class S3JKSSSLOptions(
+    var s3Url: Option[String],
+    private var jwsPassword: Option[String],
+    var awsRegion: Option[String] = None) extends SSLOptions {
+  protected lazy val clientConfiguration: ClientConfiguration = new ClientConfiguration()
+  protected lazy val s3ClientBuilder: AmazonS3ClientBuilder = AmazonS3ClientBuilder.standard()
   protected lazy val s3Client: AmazonS3 = {
     // Use Signature Version 4 to access S3 objects encrypted with SSE-KMS.
     clientConfiguration.setSignerOverride("AWSS3V4SignerType")
     s3ClientBuilder.setClientConfiguration(clientConfiguration)
     s3ClientBuilder.setCredentials(new ProfileCredentialsProvider())
-    this.awsRegion match { case Some(region) ⇒ s3ClientBuilder.withRegion(region) }
+
+    // Pattern match to set the client builder with AWS region, otherwise
+    this.awsRegion match {
+      case Some(region) ⇒ s3ClientBuilder.withRegion(region)
+      case None ⇒;
+    }
+
     s3ClientBuilder.build()
   }
-  protected lazy val clientConfiguration: ClientConfiguration = new ClientConfiguration()
-  protected lazy val s3ClientBuilder: AmazonS3ClientBuilder = AmazonS3ClientBuilder.standard()
   protected lazy val context: SSLContext = {
     val trustStore = KeyStore.getInstance("JKS")
     val s3Object = for {
-      s3URI ← Some(new AmazonS3URI(this.s3Url))
+      s3URI ← Some(new AmazonS3URI(this.s3Url.mkString))
       bucket ← Some(s3URI.getBucket)
       key ← Some(s3URI.getKey)
     } yield s3Client.getObject(bucket, key)
@@ -47,7 +51,7 @@ class S3JKSSSLOptions extends SSLOptions {
     var inputStream: S3ObjectInputStream = null
     val initSSLContext = Try {
       inputStream = s3Object.get.getObjectContent
-      trustStore.load(inputStream, this.jksPassword.toCharArray)
+      trustStore.load(inputStream, this.jwsPassword.mkString.toCharArray)
 
       val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
       trustManagerFactory.init(trustStore)
@@ -66,39 +70,17 @@ class S3JKSSSLOptions extends SSLOptions {
     }
   }
 
-  def this(s3Url: String, jksPassword: String, awsRegion: String) = {
-    this()
-    this._s3url = Some(s3Url)
-    this._jksPassword = Some(jksPassword)
-    this.awsRegion = Some(awsRegion)
-  }
+  def this(
+    s3Url: String,
+    jksPassword: String,
+    awsRegion: String) = this(Some(s3Url), Some(jksPassword), Some(awsRegion))
 
-  def this(s3Url: String, jksPassword: String) = this(s3Url, jksPassword, null)
+  def this(s3Url: String, jksPassword: String) = this(Some(s3Url), Some(jksPassword), None)
 
-  /** URL for Amazon Web Services S3 which stores the Java Key Store (JKS) file.
-    *
-    * The precedence for how the URL is resolved is first by looking up the value that was passed via the constructor
-    * or the setter, then by the environment variable key specified in the `s3URLEnvVarKey` property (defaults to
-    * `CASSANDRA_S3_TRUST_STORE_URL`).
-    */
-  def s3Url: String = {
-    val envVarS3Url: String = sys.env.get(s3URLEnvVarKey).orNull
-    this._s3url.getOrElse(envVarS3Url)
-  }
-
-  /** Sets the Amazon Web Services S3 URL that points to a Java Key Store (JKS) file.
-    *
-    * @param s3Url AWS S3 URL
-    */
-  def s3Url_=(s3Url: String) { this._s3url = Some(s3Url) }
-
-  /** Returns the JKS password in the same precedence as the `s3Url` property but the visibility is set to private.
-    * Password access is one way only for security reasons (e.g. you can set the password but not read it back out).
-    */
-  private def jksPassword: String = {
-    val envVarJksPassword: String = sys.env.get(jksPasswordEnvVarKey).orNull
-    this._jksPassword.getOrElse(envVarJksPassword)
-  }
+  def this() = this(
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_S3_TRUST_STORE_URL),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_JKS_PASSWORD)
+  )
 
   override def newSSLHandler(channel: SocketChannel): SslHandler = {
     lazy val sslEngine = {
@@ -109,4 +91,9 @@ class S3JKSSSLOptions extends SSLOptions {
 
     new SslHandler(sslEngine)
   }
+}
+
+object S3JKSSSLOptions {
+  val ENV_VAR_S3_TRUST_STORE_URL = "CASSANDRA_S3_TRUST_STORE_URL"
+  val ENV_VAR_JKS_PASSWORD = "CASSANDRA_TRUST_STORE_PASSWORD"
 }
--- src/test/scala/co/verdigris/ssl/S3JKSSSLOptionsTest.scala
+++ src/test/scala/co/verdigris/ssl/S3JKSSSLOptionsTest.scala
@@ -3,6 +3,7 @@ package co.verdigris.ssl
 import java.io.IOException
 
 import com.amazonaws.services.s3.model.AmazonS3Exception
+import com.datastax.driver.core.SSLOptions
 import io.netty.channel.socket.nio.NioSocketChannel
 import io.netty.handler.ssl.SslHandler
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
@@ -12,6 +13,22 @@ class S3JKSSSLOptionsTest extends FlatSpec with Matchers with BeforeAndAfter {
 
   before {
     sslOptions = new S3JKSSSLOptions("s3://ssllib/testkeystore.jks", "password", "us-east-1")
+  }
+
+  "Constructor" should "return an instance that implements SSLOptions interface" in {
+    sslOptions shouldBe a [SSLOptions]
+  }
+
+  it should "initialize awsRegion property to None when region is not passed in" in {
+    sslOptions = new S3JKSSSLOptions("s3://ssllib/testkeystore.jks", "password")
+    sslOptions.awsRegion shouldBe None
+  }
+
+  it should "fallback to environment variables when no arguments are passed in" in {
+    sslOptions = new S3JKSSSLOptions()
+
+    // Can only test s3Url property because JKS password is a private property for security reasons.
+    sslOptions.s3Url shouldBe sys.env.get(S3JKSSSLOptions.ENV_VAR_S3_TRUST_STORE_URL)
   }
 
   "newSSLHandler" should "return an SslHandler" in {
@@ -31,11 +48,5 @@ class S3JKSSSLOptionsTest extends FlatSpec with Matchers with BeforeAndAfter {
   it should "throw an exception when JKS password is incorrect" in {
     val sslOptions = new S3JKSSSLOptions("s3://ssllib/testkeystore.jks", "otherpassword", "us-east-1")
     an [IOException] should be thrownBy sslOptions.newSSLHandler(new NioSocketChannel())
-  }
-
-  "s3Url" should "override S3 URL value from constructor" in {
-    sslOptions.s3Url shouldBe "s3://ssllib/testkeystore.jks"
-    sslOptions.s3Url = "s3://ssllib/newkeystore.jks"
-    sslOptions.s3Url shouldBe "s3://ssllib/newkeystore.jks"
   }
 }
