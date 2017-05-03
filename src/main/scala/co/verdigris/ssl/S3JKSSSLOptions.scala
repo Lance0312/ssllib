72c7a632956890fdafd89804ab61f93aabab8feb
Andrew Jo <andrewjo@gmail.com>
2017-05-03 14:03:03 -0700

    Add support for client authentication
    Existing TrustStore allows client-to-node encryption but the KeyStore
    was needed to enable client auth.
    
    [#144739247]

--- src/main/scala/co/verdigris/ssl/S3JKSSSLOptions.scala
+++ src/main/scala/co/verdigris/ssl/S3JKSSSLOptions.scala
@@ -1,10 +1,10 @@
 package co.verdigris.ssl
 
 import java.security.KeyStore
-import javax.net.ssl.{SSLContext, TrustManagerFactory}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
 
 import com.amazonaws.ClientConfiguration
-import com.amazonaws.services.s3.model.S3ObjectInputStream
+import com.amazonaws.services.s3.model.{S3Object, S3ObjectInputStream}
 import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder, AmazonS3URI}
 import com.datastax.driver.core.SSLOptions
 import io.netty.channel.socket.SocketChannel
@@ -20,11 +20,44 @@ import scala.util.{Failure, Success, Try}
   * useful if accessing from AWS Lambda.
   */
 class S3JKSSSLOptions(
-    var s3Url: Option[String],
-    private var jwsPassword: Option[String],
+    var s3TrustStoreUrl: Option[String],
+    private var trustStorePassword: Option[String],
+    var trustStoreType: Option[String],
+    var s3KeyStoreUrl: Option[String] = None,
+    private var keyStorePassword: Option[String] = None,
+    var keyStoreType: Option[String] = None,
     var awsRegion: Option[String] = None) extends SSLOptions {
   protected lazy val clientConfiguration: ClientConfiguration = new ClientConfiguration()
   protected lazy val s3ClientBuilder: AmazonS3ClientBuilder = AmazonS3ClientBuilder.standard()
+
+  protected lazy val s3TrustStoreURI: Option[AmazonS3URI] = this.getS3URI(this.s3TrustStoreUrl)
+  protected lazy val trustStoreS3Object: Option[S3Object] = this.getS3Object(s3TrustStoreURI)
+  protected lazy val trustStoreInputStream: Option[S3ObjectInputStream] = getS3ObjectInputStream(trustStoreS3Object)
+  protected lazy val trustStore: Option[KeyStore] = this.getKeyStore(this.s3TrustStoreUrl, this.trustStoreType)
+  protected lazy val tmf: Option[TrustManagerFactory] = for {
+    ts ← this.trustStore
+    tsInputStream ← this.trustStoreInputStream
+  } yield {
+    val tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
+    ts.load(tsInputStream, this.trustStorePassword.map(_.toCharArray).orNull)
+    tmf.init(ts)
+    tmf
+  }
+
+  protected lazy val s3KeyStoreURI: Option[AmazonS3URI] = this.getS3URI(this.s3KeyStoreUrl)
+  protected lazy val keyStoreS3Object: Option[S3Object] = this.getS3Object(s3KeyStoreURI)
+  protected lazy val keyStoreInputStream: Option[S3ObjectInputStream] = getS3ObjectInputStream(keyStoreS3Object)
+  protected lazy val keyStore: Option[KeyStore] = this.getKeyStore(this.s3KeyStoreUrl, this.keyStoreType)
+  protected lazy val kmf: Option[KeyManagerFactory] = for {
+    ks ← this.keyStore
+    ksInputStream ← this.keyStoreInputStream
+  } yield {
+    val kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
+    ks.load(ksInputStream, this.keyStorePassword.map(_.toCharArray).orNull)
+    kmf.init(ks, this.keyStorePassword.map(_.toCharArray).orNull)
+    kmf
+  }
+
   protected lazy val s3Client: AmazonS3 = {
     // Use Signature Version 4 to access S3 objects encrypted with SSE-KMS.
     clientConfiguration.setSignerOverride("AWSS3V4SignerType")
@@ -34,29 +67,17 @@ class S3JKSSSLOptions(
 
     s3ClientBuilder.build()
   }
+
   protected lazy val context: SSLContext = {
-    val trustStore = KeyStore.getInstance("JKS")
-    val s3Object = for {
-      s3URI ← Some(new AmazonS3URI(this.s3Url.mkString))
-      bucket ← Some(s3URI.getBucket)
-      key ← Some(s3URI.getKey)
-    } yield s3Client.getObject(bucket, key)
-
-    var inputStream: S3ObjectInputStream = null
     val initSSLContext = Try {
-      inputStream = s3Object.get.getObjectContent
-      trustStore.load(inputStream, this.jwsPassword.mkString.toCharArray)
-
-      val trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm)
-      trustManagerFactory.init(trustStore)
-
       val sslContext = SSLContext.getInstance("TLS")
-      sslContext.init(null, trustManagerFactory.getTrustManagers, null)
+      sslContext.init(this.kmf.map(_.getKeyManagers).orNull, this.tmf.map(_.getTrustManagers).orNull, null)
       sslContext
     }
 
     // Close the stream or we'll have a memory leak on our hands.
-    if (inputStream != null) inputStream.close()
+    for (tsInputStream ← this.trustStoreInputStream) yield { tsInputStream.close() }
+    for (ksInputStream ← this.keyStoreInputStream) yield { ksInputStream.close() }
 
     initSSLContext match {
       case Success(sslContext) ⇒ sslContext
@@ -65,16 +86,104 @@ class S3JKSSSLOptions(
   }
 
   def this(
-    s3Url: String,
-    jksPassword: String,
-    awsRegion: String) = this(Some(s3Url), Some(jksPassword), Some(awsRegion))
+      s3TrustStoreUrl: Option[String],
+      trustStorePassword: Option[String],
+      trustStoreType: Option[String],
+      awsRegion: Option[String]) = {
+    this(s3TrustStoreUrl, trustStorePassword, trustStoreType, None, None, None, awsRegion)
+  }
 
-  def this(s3Url: String, jksPassword: String) = this(Some(s3Url), Some(jksPassword), None)
+  def this(
+      s3TrustStoreUrl: String,
+      trustStorePassword: String,
+      trustStoreType: String,
+      s3KeyStoreUrl: String,
+      keyStorePassword: String,
+      keyStoreType: String,
+      awsRegion: String) = this(
+    Some(s3TrustStoreUrl),
+    Some(trustStorePassword),
+    Some(trustStoreType),
+    Some(s3KeyStoreUrl),
+    Some(keyStorePassword),
+    Some(keyStoreType),
+    Some(awsRegion))
+
+  def this(
+      s3TrustStoreUrl: String,
+      trustStorePassword: String,
+      trustStoreType: String,
+      s3KeyStoreUrl: String,
+      keyStorePassword: String,
+      keyStoreType: String) = this(
+    Some(s3TrustStoreUrl),
+    Some(trustStorePassword),
+    Some(trustStoreType),
+    Some(s3KeyStoreUrl),
+    Some(keyStorePassword),
+    Some(keyStoreType),
+    None)
+
+  def this(
+      s3TrustStoreUrl: String,
+      trustStorePassword: String,
+      trustStoreType: String,
+      awsRegion: String) = this(Some(s3TrustStoreUrl), Some(trustStorePassword), Some(trustStoreType), Some(awsRegion))
+
+  def this(
+      s3TrustStoreUrl: String,
+      trustStorePassword: String,
+      trustStoreType: String) = this(Some(s3TrustStoreUrl), Some(trustStorePassword), Some(trustStoreType))
 
   def this() = this(
     sys.env.get(S3JKSSSLOptions.ENV_VAR_S3_TRUST_STORE_URL),
-    sys.env.get(S3JKSSSLOptions.ENV_VAR_JKS_PASSWORD)
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_TRUST_STORE_PASSWORD),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_TRUST_STORE_TYPE),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_S3_KEY_STORE_URL),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_KEY_STORE_PASSWORD),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_KEY_STORE_TYPE),
+    sys.env.get(S3JKSSSLOptions.ENV_VAR_S3_REGION)
   )
+
+  protected def getS3URI(url: Option[String]): Option[AmazonS3URI] = url match {
+    case Some("") | None ⇒ None
+    case Some(u) ⇒ Some(new AmazonS3URI(u))
+  }
+
+  protected def getS3Object(uri: Option[AmazonS3URI]): Option[S3Object] = Try {
+    for {
+      s3URI ← uri
+      bucket ← Some(s3URI.getBucket)
+      key ← Some(s3URI.getKey)
+    } yield {
+      s3Client.getObject(bucket, key)
+    }
+  } match {
+    case Success(s3Object) ⇒ s3Object
+    case Failure(e) ⇒ throw e
+  }
+
+  protected def getS3ObjectInputStream(s3Object: Option[S3Object]): Option[S3ObjectInputStream] = s3Object match {
+    case Some(s3Obj) ⇒ Some(s3Obj.getObjectContent)
+    case None ⇒ None
+  }
+
+  protected def getKeyStore(
+      url: Option[String],
+      keyStoreType: Option[String]): Option[KeyStore] = (url, keyStoreType) match {
+    case (Some(""), _) | (None, _) ⇒ None
+    case (Some(_), ksType) ⇒ Some(KeyStore.getInstance(ksType.getOrElse("JKS")))
+  }
+
+  protected def getTrustManagerFactory(trustStore: Option[KeyStore]): Option[TrustManagerFactory] = trustStore match {
+    case Some(_) ⇒ Some(TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm))
+    case None ⇒ None
+  }
+
+  protected def getKeyManagerFactory(keyStore: Option[KeyStore]): Option[KeyManagerFactory] = keyStore match {
+    case Some(_) ⇒ Some(KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm))
+    case None ⇒ None
+  }
 
   override def newSSLHandler(channel: SocketChannel): SslHandler = {
     lazy val sslEngine = {
@@ -89,5 +198,16 @@ class S3JKSSSLOptions(
 
 object S3JKSSSLOptions {
   val ENV_VAR_S3_TRUST_STORE_URL = "CASSANDRA_S3_TRUST_STORE_URL"
-  val ENV_VAR_JKS_PASSWORD = "CASSANDRA_TRUST_STORE_PASSWORD"
+  val ENV_VAR_TRUST_STORE_PASSWORD = "CASSANDRA_TRUST_STORE_PASSWORD"
+  val ENV_VAR_TRUST_STORE_TYPE = "CASSANDRA_TRUST_STORE_TYPE"
+  val ENV_VAR_S3_KEY_STORE_URL = "CASSANDRA_S3_KEY_STORE_URL"
+  val ENV_VAR_KEY_STORE_PASSWORD = "CASSANDRA_KEY_STORE_PASSWORD"
+  val ENV_VAR_KEY_STORE_TYPE = "CASSANDRA_KEY_STORE_TYPE"
+  val ENV_VAR_S3_REGION = "CASSANDRA_S3_REGION"
+
+  class Builder {
+
+  }
+
+  def builder(): Builder = new Builder
 }
